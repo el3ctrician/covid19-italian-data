30a7f649891d7acc1526ff8e6211546d5190b1aa
el3ctrician <elbadriahmad@gmail.com>
2020-03-29 14:47:20 +0200

    better presentation for graphs

--- basic_plot.py
+++ basic_plot.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import MultiCursor
 import datetime as dt
 import json
+import matplotlib.ticker as mtick
+
 dates = []
 discovered_cases = []
 total_cases = []
@@ -41,17 +43,26 @@ fig, axs = plt.subplots(3, 2)
 fig.suptitle('COVID19 Italian situation, last update '+today.strftime("%d-%m-%Y , %H:%M"), fontsize=16)
 fig.set_size_inches(18.5, 10.5)
 axs[0,0].plot(dates,tested,'-oc')
-axs[0,0].set_title("Daily tested people")
+axs[0,0].set_title("Daily Tested Cases")
 axs[0, 1].plot(dates,discovered_cases,'-ob')
-axs[0,1].set_title("Daily new cases")
+axs[0,1].set_title("Daily Positive Cases")
 axs[1,0].plot(dates,death,'-or')
 axs[1,0].set_title("Daily Death Cases")
 axs[1,1].plot(dates,recovered_cases,'-og')
-axs[1,1].set_title("Daily recovered people")
+axs[1,1].set_title("Daily Recovered Cases")
 axs[2,0].plot(dates,positive_perct,'-om')
-axs[2,0].set_title("Percentage of positive people")
+axs[2,0].set_title("Percentage of Positive Cases")
+axs[2,0].yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1))
 axs[2,1].plot(dates,lethality,'-ok',lethality_on_pos,'-or')
 axs[2,1].set_title("Lethality")
+axs[2,1].annotate('For Positive Cases', xy=(15, 0.071), xytext=(2, 0.1),
+            arrowprops=dict(facecolor='red', shrink=0.01, width=2))
+axs[2,1].annotate('For Tested Cases', xy=(19, 0.024), xytext=(22, 0.06),
+            arrowprops=dict(facecolor='black', shrink=0.01, width=2))
+axs[2,1].yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1))
+for plot in axs.flatten():
+  plot.set_xlabel("Days starting from 23/02/2020")
+plt.subplots_adjust(wspace=0.2, hspace=0.4)
 # multi = MultiCursor(fig.canvas,axs.flatten(),horizOn=f, color='r', lw=1)
 plt.savefig('data.svg', format='svg', dpi=1200)
 plt.show()
