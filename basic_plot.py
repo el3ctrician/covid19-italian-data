e57703dea7673be1092bdad2359711089d1c8828
el3ctrician <elbadriahmad@gmail.com>
2020-04-02 15:01:10 +0200

    removed dependency on sub modules by parsing the link directly

--- basic_plot.py
+++ basic_plot.py
@@ -4,6 +4,8 @@ from matplotlib.widgets import MultiCursor
 import datetime as dt
 import json
 import matplotlib.ticker as mtick
+import requests
+PCM_DPC_URL = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-covid19-ita-andamento-nazionale.json"
 
 dates = []
 discovered_cases = []
@@ -20,29 +22,31 @@ last_recovered = 0
 last_tested = 0
 last_death = 0
 start_time = dt.datetime(2020, 2, 23)
-with open('./pcm-dpc-data/dati-json/dpc-covid19-ita-andamento-nazionale.json') as json_file:
-    data = json.load(json_file)
-    for obj in data:
-      date=dt.datetime.strptime(obj['data'],'%Y-%m-%dT%H:%M:%S')-start_time
-      dates.append(date.days)
-      discovered_cases.append(int(obj['totale_casi']) - last_cases)
-      recovered_cases.append(obj['dimessi_guariti']-last_recovered)
-      tested.append(obj['tamponi']-last_tested)
-      death.append(obj['deceduti']-last_death)
-      positive_perct.append(discovered_cases[-1]/tested[-1])
-      lethality.append(obj['deceduti']/obj['tamponi'])
-      lethality_on_pos.append(obj['deceduti']/int(obj['totale_casi']))
-      last_cases = obj['totale_casi']
-      last_tested = obj['tamponi']
-      last_recovered = obj['dimessi_guariti']
-      last_death = obj['deceduti']
+
+response = requests.get(PCM_DPC_URL)
+
+
+for obj in response.json():
+  date=dt.datetime.strptime(obj['data'],'%Y-%m-%dT%H:%M:%S').strftime("%m/%d")
+  dates.append(date)
+  discovered_cases.append(int(obj['totale_casi']) - last_cases)
+  recovered_cases.append(obj['dimessi_guariti']-last_recovered)
+  tested.append(obj['tamponi']-last_tested)
+  death.append(obj['deceduti']-last_death)
+  positive_perct.append(discovered_cases[-1]/tested[-1])
+  lethality.append(obj['deceduti']/obj['tamponi'])
+  lethality_on_pos.append(obj['deceduti']/int(obj['totale_casi']))
+  last_cases = obj['totale_casi']
+  last_tested = obj['tamponi']
+  last_recovered = obj['dimessi_guariti']
+  last_death = obj['deceduti']
 
 today = dt.datetime.now()
 
 fig, axs = plt.subplots(3, 2)
 fig.suptitle('COVID19 Italian situation, last update '+today.strftime("%d-%m-%Y , %H:%M"), fontsize=16)
 fig.set_size_inches(18.5, 10.5)
-axs[0,0].plot(dates,tested,'-oc')
+axs[0,0].plot(dates,tested,'-oc',ms=4)
 axs[0,0].set_title("Daily Tested Cases")
 axs[0,1].plot(dates,death,'-or')
 axs[0,1].set_title("Daily Death Cases")
@@ -61,8 +65,9 @@ axs[2,1].annotate('For Tested Cases', xy=(19, 0.024), xytext=(22, 0.06),
             arrowprops=dict(facecolor='black', shrink=0.01, width=2))
 axs[2,1].yaxis.set_major_formatter(mtick.PercentFormatter(xmax=1))
 for plot in axs.flatten():
-  plot.set_xlabel("Days starting from 23/02/2020")
-plt.subplots_adjust(wspace=0.2, hspace=0.4)
-# multi = MultiCursor(fig.canvas,axs.flatten(),horizOn=f, color='r', lw=1)
+  plot.tick_params(axis='x', which='major', labelsize=8, rotation=45)
+
+plt.subplots_adjust(wspace=0.1, hspace=0.4)
+multi = MultiCursor(fig.canvas,axs.flatten(),horizOn=False, color='r', lw=1)
 plt.savefig('data.svg', format='svg', dpi=1200)
 plt.show()
